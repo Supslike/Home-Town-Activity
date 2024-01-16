80d027df9f700a85001a6530452699f1f6dd316f
Supslike <80964103+Supslike@users.noreply.github.com>
2024-01-16 17:39:11 +0800

    uploaded files

---
+++ scripts/reveal.js
@@ -0,0 +1,25 @@
+let jeepney = document.getElementById("jeep");
+
+// Function to reveal elements based on scroll position
+function reveal() {
+    // Selecting elements with class "reveal", "warning", and "animate"
+    var reveals = document.querySelectorAll(".reveal");
+    jeepney.style.transform = `translate(-${window.scrollY * 1.5}px)`
+
+    // Loop through elements with class "reveal"
+    for (var i = 0; i < reveals.length; i++) {
+        var windowHeight = window.innerHeight;
+        var elementTop = reveals[i].getBoundingClientRect().top;
+        var elementVisible = 150;
+  
+        // Check if the element is within the visible range
+        if (elementTop < windowHeight - elementVisible) {
+            reveals[i].classList.add("active");
+        } else {
+            reveals[i].classList.remove("active");
+        }
+    }
+}
+  
+// Event listener for scroll event, calling the reveal function
+window.addEventListener("scroll", reveal);
